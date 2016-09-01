f2d1eef657877b656ed3d4f0ce0755a410375419
Brenda Chan <brchan@pivotal.io>
2016-09-01 15:08:03 -0400

    Update uploaded bosh releases to match those for PCF 1.6, also update diego.yml.mustache for the same
    [#129533489]
    
    Signed-off-by: Sam Coward <scoward@pivotal.io>

--- azure/devbox_upload_bosh_releases.sh
+++ azure/devbox_upload_bosh_releases.sh
@@ -5,8 +5,8 @@ bosh target 10.0.0.4
 bosh login admin PASSWORDHERE
 
 bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-mysql-release?v=24 --skip-if-exists
-bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-release?v=231 --skip-if-exists
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release?v=0.333.0 --skip-if-exists
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=36 --skip-if-exists
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/diego-release?v=0.1454.0 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry/cf-release?v=225 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release?v=0.338.0 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/etcd-release?v=18 --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/diego-release?v=0.1446.0 --skip-if-exists
 bosh upload stemcell https://bosh.io/d/stemcells/bosh-azure-hyperv-ubuntu-trusty-go_agent?v=3232.11 --skip-if-exists
